3c1106d371f11436954d4f9b3facc849ccc10d54
chembeu <simon.shanga11@gmail.com>
2025-07-16 14:06:59 +0300

    authetication folder

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,13 +1,14 @@
 # auth/routes.py
 from flask import render_template, redirect, url_for, flash
-from . import auth_bp
-from ..forms import RegistrationForm
+from . import auth
+from ..forms import DJForm
 
-@auth_bp.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
+
+@auth.route('/add_dj', methods=['GET', 'POST'])
+def add_dj():
+    form = DJForm()
     if form.validate_on_submit():
-        # Add user registration logic here
-        flash('Registration successful!', 'success')
+        # Add DJ creation logic here
+        flash('DJ added successfully!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', form=form)
+    return render_template('add_dj.html', form=form)
